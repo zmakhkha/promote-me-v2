5dab632eba2e69ca070098f1ea113aae777bd009
zakaria <jcaladic@gmail.com>
2025-02-20 23:28:39 +0100

    working   on nonauth page

--- front/src/app/omegle/page.tsx
+++ front/src/app/omegle/page.tsx
@@ -1,33 +1,61 @@
 "use client";
 
-import { Flex, IconButton } from "@chakra-ui/react";
-import React from "react";
+import { Grid, GridItem } from "@chakra-ui/react";
+import React, { useEffect } from "react";
+import Sidebar from "@/common/Sidebar";
 import useColorModeStyles from "@/utils/useColorModeStyles";
-import OmegleScreen from "@/components/chat/OmegleChatScreen";
-import { BellIcon, MoonIcon, SunIcon } from "@chakra-ui/icons";
-
+import MainInstagram from "@/components/instagram/mainInstagram";
+import Header from "@/common/Header";
+import { useRouter } from "next/navigation";
+import { checkAuthTokens } from "@/services/axios/checkAuthTokens";
+import socketConnect from "@/services/axios/socketConnect";
+import OmegleChatScreen from "@/components/chat/OmegleChatScreen";
+import NonAuthHeader from "@/common/NonAuthHeader";
 
 const HomePage = () => {
   const { bg, textColor, navBgColor } = useColorModeStyles();
-  const { toggleColorMode } = useColorModeStyles();
-
   return (
-    <Flex
+    <Grid
+      templateAreas={{
+        base: `"header"
+               "main"`,
+        md: `"header header"
+             "nav main"`,
+      }}
+      gridTemplateRows={{ base: "auto 1fr", md: "50px 1fr" }}
+      gridTemplateColumns={{ base: "1fr", md: "50px 1fr" }}
       height="100vh"
-      align="center"
-      justify="center"
-      bg={navBgColor}
+      gap="0.5"
       color={textColor}
+      fontWeight="bold"
     >
-      <IconButton
-        icon={bg === "gray.200" ? <MoonIcon /> : <SunIcon />}
-        aria-label="Toggle Color Mode"
-        variant="ghost"
-        onClick={toggleColorMode}
-        ml={4}
-      />
-      <OmegleScreen />
-    </Flex>
+      {/* Header Section */}
+      <GridItem area="header" position="sticky" top="0" zIndex="2" bg={bg}>
+        <NonAuthHeader />
+      </GridItem>
+
+      {/* Sidebar Section */}
+      <GridItem
+        area="nav"
+        bg={navBgColor}
+        display={{ base: "none", md: "block" }}
+      >
+        <Sidebar />
+      </GridItem>
+
+      {/* Main Content Section */}
+      <GridItem
+        area="main"
+        position="sticky"
+        zIndex="1"
+        overflowY="auto"
+        pl="2"
+        bg={navBgColor}
+      >
+        {/* Placeholder for Main Content */}
+        <OmegleChatScreen />
+      </GridItem>
+    </Grid>
   );
 };
 
